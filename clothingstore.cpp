d9ea7769d98b3cfdeee455e14a8faff876b4fb0b
annalavryk <anna.lavryk2007@gmail.com>
2026-04-02 23:20:55 +0300

    Update clothingstore.cpp

--- clothingstore.cpp
+++ clothingstore.cpp
@@ -6,40 +6,83 @@ int main() {
     string items[] = { "T-shirt", "Jeans", "Jacket", "Sneakers", "Hoodie" };
     double prices[] = { 20.0, 45.0, 80.0, 60.0, 35.0 };
 
+    int day, month;
+    char dot;
+
+    cout << "Enter date: ";
+    cin >> day >> dot >> month;
+
+    
+    int discountIndex = (day + month) % 5;
+
+    cout << "\n=== TODAY'S DISCOUNT ===\n";
+    cout << "Discount product: " << items[discountIndex] << " (-10%)\n";
+
+    cout << "\n=== STORE ===\n";
+
+    for (int i = 0; i < 5; i++) {
+        cout << i + 1 << ". " << items[i]
+            << " - $" << prices[i];
+
+        if (i == discountIndex) {
+            cout << " (-10% TODAY!)";
+        }
+
+        cout << endl;
+    }
+
     int choice;
     double total = 0;
 
-    cout << "=== CLOTHING STORE ===" << endl;
-
-    for (int i = 0; i < 5; i++) {
-        cout << i + 1 << ". " << items[i] << " - $" << prices[i] << endl;
-    }
-
-    cout << endl;
-    cout << "Choose an item (0 to finish shopping)" << endl;
+    cout << "\nChoose item (0 to finish)\n";
 
     while (true) {
         cout << "Your choice: ";
         cin >> choice;
 
-        if (choice == 0) {
-            break;
-        }
+        if (choice == 0) break;
 
         if (choice >= 1 && choice <= 5) {
 
-            cout << "You selected: " << items[choice - 1]
-                << " - $" << prices[choice - 1] << endl;
+            double price = prices[choice - 1];
 
-            total += prices[choice - 1];
+            
+            if (choice - 1 == discountIndex) {
+                price *= 0.9;
+                cout << "Day discount applied! ";
+            }
+
+            cout << items[choice - 1] << " - $" << price << endl;
+
+            total += price;
         }
         else {
-            cout << "Invalid choice!" << endl;
+            cout << "Invalid choice!\n";
         }
     }
 
-    cout << endl;
-    cout << "Total price: $" << total << endl;
+ 
+    string promo;
+    cout << "\nEnter promo code (or 'no'): ";
+    cin >> promo;
+
+    if (promo == "SALE10") {
+        total *= 0.9;
+        cout << "Promo -10% applied!\n";
+    }
+    else if (promo == "SALE20") {
+        total *= 0.8;
+        cout << "Promo -20% applied!\n";
+    }
+    else if (promo == "SUPER30") {
+        total *= 0.7;
+        cout << "Promo -30% applied!\n";
+    }
+    else if (promo != "no") {
+        cout << "Invalid promo code!\n";
+    }
+
+    cout << "\nTOTAL: $" << total << endl;
 
     return 0;
 }
